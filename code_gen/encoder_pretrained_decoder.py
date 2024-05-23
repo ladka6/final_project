141bbf3cd1b729565c7247f37190eb5485a98fbc
Ege Erdal <ege-erdal101@hotmail.com>
2024-05-23 18:24:55 +0300

    added generation

--- code_gen/encoder_pretrained_decoder.py
+++ code_gen/encoder_pretrained_decoder.py
@@ -110,3 +110,23 @@ trainer = Seq2SeqTrainer(
 print("\nTraining the model\n")
 trainer.train()
 model.save_pretrained(f"./saved_models/encoder_pretrained_decoder{12}")
+print("\nGenerating code for testing the model out\n")
+for i in range(3):
+    text = """
+        public class Test {
+            public static void main(String[] args){
+                System.out.println("Hello World");
+            }
+        }
+        """
+
+    inputs = tokenizer(
+        text, return_tensors="pt", padding=True, truncation=True, max_length=512
+    )
+
+    input_ids = inputs.input_ids
+    input_ids = input_ids.to(model.device)
+
+    outputs = model.generate(inputs=input_ids)
+    out = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(out)
